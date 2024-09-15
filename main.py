97d63be0f464108dd9a393fe5ba23f4595854608
Babahasko <zakutni4ek@gmail.com>
2024-09-15 21:51:55 +0300

    use orjson

--- main.py
+++ main.py
@@ -1,28 +1,32 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
 import uvicorn
 from dotenv import load_dotenv
 from api import router
 from core.config import settings
-from core.models import db_helper, Base
+from core.models import db_helper
 
 load_dotenv()
 
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    #startup
+    # startup
     yield
-    #shutdown
+    # shutdown
     await db_helper.dispose()
 
+
 main_app = FastAPI(
+    default_response_class=ORJSONResponse,
     lifespan=lifespan,
 )
 main_app.include_router(
     router,
-#   prefix=settings.api.prefix,
+    #   prefix=settings.api.prefix,
 )
 
 if __name__ == "__main__":
-    uvicorn.run(main_app, host = settings.run.host, port =settings.run.port)
+    uvicorn.run(main_app, host=settings.run.host, port=settings.run.port)
